2d7aca3ade2a4e4f735c0e70f94bd165a3dc41fa
Bhola nath Sarkar <sarkaraditya817@gmail.com>
2024-02-28 23:47:24 +0530

    complete the left side box

--- Frontend/src/layouts/dashboard/index.js
+++ Frontend/src/layouts/dashboard/index.js
@@ -1,5 +1,5 @@
-import { Avatar, Box, Divider, IconButton, Stack } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
+import { useTheme, styled } from "@mui/material/styles";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
@@ -7,16 +7,61 @@ import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data";
 import { Gear } from "phosphor-react";
 import { faker } from "@faker-js/faker";
+import useSettings from "../../hooks/useSettings";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 28,
+  height: 16,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(12px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 16 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 const DashboardLayout = () => {
   const theme = useTheme();
 
   const [selected, setSelected] = useState(0);
-  console.log(theme);
-
+  // console.log(theme);
+  const { onToggleMode } = useSettings();
   return (
     <>
-      <Box
+     <Box
         p={2}
         sx={{
           backgroundColor: theme.palette.background.paper,
@@ -28,7 +73,8 @@ const DashboardLayout = () => {
         <Stack
           direction="column"
           alignItems={"center"}
-          sx={{ width: "100%" }}
+          justifyContent={"space-between"}
+          sx={{ height: "100%" }}
           spacing={3}
         >
           <Stack alignItems={"center"} spacing={3}>
@@ -69,7 +115,7 @@ const DashboardLayout = () => {
                     onClick={() => {
                       setSelected(el.index);
                     }}
-                    sx={{ width: "max-content", color: "#000" }}
+                    sx={{ width: "max-content", color: theme.palette.mode === "light"? "#000": theme.palette.text.primary }}
                     key={el.index}
                   >
                     {" "}
@@ -96,19 +142,27 @@ const DashboardLayout = () => {
                   onClick={() => {
                     setSelected(3);
                   }}
-                  sx={{ width: "max-content", color: "#000" }}
+                  sx={{ width: "max-content",  color: theme.palette.mode === "light"? "#000": theme.palette.text.primary }}
                 >
                   <Gear size={32} />
                 </IconButton>
               )}
             </Stack>
           </Stack>
+          {/* switch */}
 
-          <Stack>
+          <Stack spacing={3} alignItems={"center"}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
             <Avatar src={faker.image.avatar()} />
           </Stack>
         </Stack>
       </Box>
+     
       <Outlet />
     </>
   );
