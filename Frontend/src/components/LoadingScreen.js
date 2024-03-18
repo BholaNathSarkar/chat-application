63feb497d4c9ed5aac59555e723cb2bf66de476b
Bhola nath Sarkar <sarkaraditya817@gmail.com>
2024-03-18 22:11:05 +0530

    add lodder in the middle of the screen

--- Frontend/src/components/LoadingScreen.js
+++ Frontend/src/components/LoadingScreen.js
@@ -1,7 +1,20 @@
+import { Box, Grid, Stack, Typography } from "@mui/material";
+
 import React from "react";
 
 const LoadingScreen = () => {
-  return <>Loading...</>;
+  return (
+    <>
+      <Stack
+        direction={"row"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        sx={{ width: "100%", height: "100vh" }}
+      >
+        <Typography>Loading....</Typography>
+      </Stack>
+    </>
+  );
 };
 
 export default LoadingScreen;
